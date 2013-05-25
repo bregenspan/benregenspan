142f017c1eed71330288965b4efa22cdb28e0f1c
Ben Regenspan <bregenspan@gmail.com>
2013-05-25 15:06:49 -0400

    Add Gruntfile + organize source

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      my_target: {
+          files: {
+              'build/main.js': [
+                  'src/js/polyline.js',
+                  'src/js/lib/RaF_polyfill.js',
+                  'src/js/lib/skrollr.min.js',
+                  'src/js/lib/skrollr.mobile.min.js'
+              ]
+          }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+            { expand: true, cwd: 'src/', src: ['**.html', 'fonts', 'fonts/**', 'img', 'img/**'], dest: 'build/'}
+        ]
+      }
+    },
+    watch: {
+        scripts: {
+            files: 'src/**.js',
+            tasks: ['uglify']
+        },
+        other: {
+            files: 'src/**',
+            tasks: ['copy']
+        }
+     }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['uglify', 'copy']);
+};
