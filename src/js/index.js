5c3b0f3a2990b070b163b815f2ecf9225f825a43
Ben Regenspan <bregenspan@gmail.com>
2013-05-25 16:47:19 -0400

    Break out Skrollr's junk from the document, apply via JS

--- src/js/index.js
+++ src/js/index.js
@@ -7,19 +7,24 @@
 (function () {
     'use strict';
 
+    var doc = document;
+
+    function $(id) { return doc.getElementById(id); }
+
     // Pretty Webfonts
     window.WebFontConfig = {
         google: { families: [ 'Open+Sans:400,600', 'Noto+Serif::latin'] }
     };
     (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        var wf = doc.createElement('script');
+        wf.src = ('https:' == doc.location.protocol ? 'https' : 'http') +
           '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
         wf.type = 'text/javascript';
         wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
+        var s = doc.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(wf, s);
     })();
+
 
     // Stupid Pinterest widget
     (function(d){
@@ -28,9 +33,29 @@
         p.async = true;
         p.src = '//assets.pinterest.com/js/pinit.js';
         f.parentNode.insertBefore(p, f);
-    }(document));
- 
+    }(doc));
+
+
+    var progressBar = doc.createElement('div');
+    progressBar.className = 'doodad-track';
+    var progressBarNub = doc.createElement('div');
+    progressBarNub.classname = 'doodad';
+    progressBar.appendChild(progressBarNub);
+    $('container').appendChild(progressBar);
+
+
     // Nifty on-scroll effects
+    var title = $('title');
+    title.setAttribute('data-0', 'opacity:1; top:22px; transform:rotate(9deg)');
+    title.setAttribute('data-end', 'opacity:0; top:500px; transform:rotate(130deg);');
+
+    var sidebar = $('sidebar');
+    sidebar.setAttribute('data-0', 'top:120px');
+    sidebar.setAttribute('data-end', 'top:10px');
+
+    progressBarNub.setAttribute('data-0', 'left:0%');
+    progressBarNub.setAttribute('data-end', 'left:100%');
+
     skrollr.init();
 
 }());
