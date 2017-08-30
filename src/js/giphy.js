04aed6434b5c7f1d34144382d8ee4b4356c91413
Ben Regenspan <bregenspan@gmail.com>
2017-08-30 15:04:41 -0400

    More ES6ification, use fetch ponyfill

--- src/js/giphy.js
+++ src/js/giphy.js
@@ -1,50 +1,48 @@
-/*global define*/
+import fetch from 'unfetch';
 
-define(['lib/xdr'], function(xdr) {
-    'use strict';
+const G = function (apiKey) {
+  this.apiKey = apiKey;
+};
 
-    var G = function (apiKey) {
-        this.apiKey = apiKey;
-    };
+G.prototype.getRandomMrDiv = function (callback) {
+  if (this._requesting) {
+    return false;
+  }
+  if (!this.data) {
+    const ENDPOINT = 'http://api.giphy.com/v1/gifs/artists?api_key=' + this.apiKey + '&username=mrdiv&limit=100';
+    this._requesting = true;
+    fetch(ENDPOINT)
+      .then(response => response.json())
+      .then((data) => {
+        this.data = data;
+        this._requesting = false;
+        this._getRandomGIF(callback);
+      })
+      .catch((error) => {
+        console.error(error);
+        this._requesting = false;
+      });
+  } else {
+    this._getRandomGIF(callback);
+  }
+};
 
-    G.prototype.getRandomMrDiv = function (callback) {
-        var client = this;
-        if (this._requesting) {
-            return false;
-        }
-        if (!this.data) {
-            var ENDPOINT = 'http://api.giphy.com/v1/gifs/artists?api_key=' + this.apiKey + '&username=mrdiv&limit=100';
-            this._requesting = true;
-            xdr(ENDPOINT, 'GET', null, function (response) {
-                client.data = JSON.parse(response);
-                client._requesting = false;
-                client._getRandomGIF(callback);
-            }, function () {
-                client._requesting = false;   
-            });
-        } else {
-            client._getRandomGIF(callback);
-        }
-    };
+G.prototype._getRandomGIF = function (callback) {
+  if (!this.data && this.data.data) {
+    console.error('No GIFS found :(');
+    return;
+  }
 
-    G.prototype._getRandomGIF = function (callback) {
-        if (!this.data && this.data.data) {
-            console.error('No GIFS found :(');
-            return;
-        }
+  const count = this.data.data.length;
+  const min = 0;
+  const max = count - 1;
+  const randIndex = Math.floor(Math.random() * (max - min) + min);
+  const result = this.data.data[randIndex];
+  const gif = {
+    'img': result.images.original.url,
+    'url': result.url
+  };
+  callback(gif);
+};
 
-        var count = this.data.data.length;
-        var min = 0,
-            max = count - 1,
-            randIndex = Math.floor(Math.random() * (max - min) + min),
-            result = this.data.data[randIndex];
-        var gif = {
-            'img': result.images.original.url,
-            'url': result.url
-        };
-        callback(gif);
-    };
-
-    return G;
-
-});
+export default G;
