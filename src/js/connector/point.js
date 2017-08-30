979ed55b0b8cb28a8b98f51e911320489247b046
Ben Regenspan <bregenspan@gmail.com>
2017-08-30 16:53:32 -0400

    Linting

--- src/js/connector/point.js
+++ src/js/connector/point.js
@@ -1,25 +1,18 @@
-/*global define*/
+export default class Point {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-define(function () {
-    'use strict';
+  greaterThan (point) {
+    return (this.y > point.y || this.x > point.x);
+  }
 
-    var Point = function (x, y) {
-        this.x = x;
-        this.y = y;
-    };
+  equals (point) {
+    return (this.y === point.y && this.x === point.x);
+  }
 
-    Point.prototype.greaterThan = function (point) {
-        return (this.y > point.y || this.x > point.x);
-    };
-
-    Point.prototype.equals = function (point) {
-        return (this.y === point.y && this.x === point.x);
-    };
-
-    Point.prototype.clone = function () {
-        return new Point(this.x, this.y);
-    };
-
-    return Point;
-
-});
+  clone () {
+    return new Point(this.x, this.y);
+  }
+}
